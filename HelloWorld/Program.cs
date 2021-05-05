9c78d9729c57c72430b1cbe3908cf39024148267
leranizolia <leranizolya@yandex.ru>
2021-05-05 23:01:12 +0300

    First commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using classes;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Valeriia", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+            // Test that the initial balances must be positive.
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+            //TODO: ask about try catch and if throw
+            // Test for a negative balance.
+            try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine(account.GetAccountHistory());
+
+            var giftCard = new GiftCardAccount("gift card", 100, 50);
+            giftCard.MakeWithdrawal(20, DateTime.Now, "get expensive cofee");
+            giftCard.MakeWithdrawal(50, DateTime.Now, "buy groceries");
+            giftCard.PerformMonthEndTransactions();
+            //can make additional deposits;
+            giftCard.MakeDeposit(27.50m, DateTime.Now, "add some additional spending money");
+            Console.WriteLine(giftCard.GetAccountHistory());
+
+            var savings = new InterestEarningAccount("savings account", 10000);
+            savings.MakeDeposit(750, DateTime.Now, "save some money");
+            savings.MakeDeposit(1250, DateTime.Now, "Add more savings");
+            savings.MakeWithdrawal(250, DateTime.Now, "Needed to pay monthly bills");
+            savings.PerformMonthEndTransactions();
+            Console.WriteLine(savings.GetAccountHistory());
+
+            var lineofCredit = new LineOfCreditAccount("line of credit", 0, 2000);
+            // How much is too much to borrow?
+            lineofCredit.MakeWithdrawal(1000m, DateTime.Now, "Take out monthly advance");
+            lineofCredit.MakeDeposit(50m, DateTime.Now, "Pay back small amount");
+            lineofCredit.MakeWithdrawal(5000m, DateTime.Now, "Emergency funds for repairs");
+            lineofCredit.MakeDeposit(150m, DateTime.Now, "Partial restoration on repairs");
+            lineofCredit.PerformMonthEndTransactions();
+            Console.WriteLine(lineofCredit.GetAccountHistory());
+        }
+    }
+}
